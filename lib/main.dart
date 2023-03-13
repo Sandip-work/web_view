ca0d3eafdd52e558b2098f14dc45b4780e204916
Nirali-Work <100585991+Yograj-Work@users.noreply.github.com>
2023-03-13 16:35:36 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:web_view/menu.dart';                               // ADD
+import 'package:web_view/navigation_controls.dart';
+import 'package:web_view/web_view_stack.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+       debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://flutter.dev'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+        actions: [
+          NavigationControls(controller: controller),
+          Menu(controller: controller),               // ADD
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+    );
+  }
+}
